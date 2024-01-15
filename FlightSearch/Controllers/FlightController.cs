1cdad139009808d9fbfc6b3bf72d5c2df35646c9
rahma <rahmanmajeed783@gmail.com>
2024-01-15 18:06:24 +0500

    ...

--- FlightSearch/Controllers/FlightController.cs
+++ FlightSearch/Controllers/FlightController.cs
@@ -29,15 +29,18 @@ namespace FlightSearch.Controllers
                 {
                     using (var httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(60) })
                     {
-                        var uriBuilder = new UriBuilder("http://124.29.220.210:4005/api/OneWayTrip")
-                        {
-                            Query = $"Origin={model.From}&Destination={model.To}&Departure={model.Departure}" +
-                                    $"&Adults={model.Adults}&Children={model.Children}&Infant={model.Infant}" +
-                                    $"&ReturnType=json&UserName=intelli&ApiPassword=786786&ClientID=1" +
-                                    $"&DirectOnly={model.Direct}&ShowAlternateGrid=False"
-                        };
+                        //var apiUrl = "http://124.29.220.210:4005/api/OneWayTrip?Origin=LHE&Destination=DXB&Departure=2024-12-27&Adults=1&Children=0&Infant=0&ReturnType=json&UserName=intelli&ApiPassword=786786&ClientID=1&DirectOnly=True&ShowAlternateGrid=False";
 
-                        using (var response = await httpClient.GetAsync(uriBuilder.Uri))
+                        string url = $"http://124.29.220.210:4005/api/OneWayTrip?" +
+                                     $"Origin={model.From}" +
+                                     $"&Destination={model.To}" +
+                                     $"&Departure={model.Departure}" +
+                                     $"&Adults={model.Adults}" +
+                                     $"&Children={model.Children}" +
+                                     $"&Infant={model.Infant}&ReturnType=json&UserName=intelli&ApiPassword=786786&ClientID=1" +
+                                     $"&DirectOnly={model.Direct}&ShowAlternateGrid=False";
+
+                        using (var response = await httpClient.GetAsync(url))
                         {
                             if (response.IsSuccessStatusCode)
                             {
