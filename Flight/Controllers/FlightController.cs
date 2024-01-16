72904e0c72f17d23ac8f006a6cc9c9b84749ec89
Abdul Rahman <rahmanmajeed783@gmail.com>
2024-01-16 21:36:49 +0500

    ////

--- Flight/Controllers/FlightController.cs
+++ Flight/Controllers/FlightController.cs
@@ -13,9 +13,39 @@ namespace FlightSearch.Controllers
         }
 
 
-        public IActionResult FlightDetails()
+        public async Task<IActionResult> FlightDetails()
         {
-            return View();
+            ResponseBase<SearchResponse> response = new ResponseBase<SearchResponse>();
+
+            try
+            {
+                using (var httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(60) })
+                {
+                    var apiUrl = "http://124.29.220.210:4005/api/OneWayTrip?Origin=LHE&Destination=DXB&Departure=2024-12-27&Adults=1&Children=0&Infant=0&ReturnType=json&UserName=intelli&ApiPassword=786786&ClientID=1&DirectOnly=True&ShowAlternateGrid=False";
+
+
+                    using (var ApiResponse = await httpClient.GetAsync(apiUrl))
+                    {
+                        if (ApiResponse.IsSuccessStatusCode)
+                        {
+                            string data = await ApiResponse.Content.ReadAsStringAsync();
+                            response.Data = JsonSerializer.Deserialize<SearchResponse>(data);
+                        }
+                        else
+                        {
+                            //string errorResponse = await response.Content.ReadAsStringAsync();
+                            // Console.WriteLine($"Error: {response.StatusCode}, {errorResponse}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            return View(response);
+
         }
 
         public async Task<IActionResult> FlightSearch(FlightRequest model)
@@ -29,14 +59,6 @@ namespace FlightSearch.Controllers
                     {
                         var apiUrl = "http://124.29.220.210:4005/api/OneWayTrip?Origin=LHE&Destination=DXB&Departure=2024-12-27&Adults=1&Children=0&Infant=0&ReturnType=json&UserName=intelli&ApiPassword=786786&ClientID=1&DirectOnly=True&ShowAlternateGrid=False";
 
-                        //string url = $"http://124.29.220.210:4005/api/OneWayTrip?" +
-                        //             $"Origin={model.From}" +
-                        //             $"&Destination={model.To}" +
-                        //             $"&Departure={model.Departure}" +
-                        //             $"&Adults={model.Adults}" +
-                        //             $"&Children={model.Children}" +
-                        //             $"&Infant={model.Infant}&ReturnType=json&UserName=intelli&ApiPassword=786786&ClientID=1" +
-                        //             $"&DirectOnly={model.Direct}&ShowAlternateGrid=False";
 
                         using (var ApiResponse = await httpClient.GetAsync(apiUrl))
                         {
